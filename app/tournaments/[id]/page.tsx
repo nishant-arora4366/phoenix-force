b8a09d440cb90e7d52dde1be81503ce1453e2852
nishant-arora4366 <nishantarora1998@gmail.com>
2025-10-09 18:38:16 +0530

    Change Pending Approval icon to clipboard/document icon to differentiate from Waitlist clock icon

--- app/tournaments/[id]/page.tsx
+++ app/tournaments/[id]/page.tsx
@@ -1726,7 +1726,7 @@ export default function TournamentDetailsPage() {
                             </div>
                             <div className="w-10 h-10 bg-[#CEA17A]/20 rounded-full flex items-center justify-center">
                               <svg className="w-5 h-5 text-[#CEA17A]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                               </svg>
                             </div>
                           </div>
