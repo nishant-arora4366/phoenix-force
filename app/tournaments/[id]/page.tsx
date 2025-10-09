e0a62c40cb9edd40e6270817e62c69239d9d1f31
nishant-arora4366 <nishantarora1998@gmail.com>
2025-10-09 19:08:29 +0530

    Add warning icon and yellow text for 'No user account linked' message in Register Players dialog

--- app/tournaments/[id]/page.tsx
+++ app/tournaments/[id]/page.tsx
@@ -2570,7 +2570,12 @@ export default function TournamentDetailsPage() {
                                       ? `${player.user.firstname} ${player.user.lastname}` 
                                       : player.user.username || player.user.email
                                   ) : (
-                                    <span className="text-orange-600">No user account linked</span>
+                                    <div className="flex items-center space-x-1">
+                                      <svg className="w-4 h-4 text-yellow-500" fill="currentColor" viewBox="0 0 20 20">
+                                        <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                                      </svg>
+                                      <span className="text-yellow-500 text-xs">No user account linked</span>
+                                    </div>
                                   )}
                                 </div>
                               </div>
